26a8a3a4fee88b9896bd374836717916254be630
Don Hamilton III <donhamiltoniii@gmail.com>
2020-01-07 21:01:13 -0500

    Initial Commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ description, siteTitle }) => (
+  <header>
+    <h1 style={{ margin: 0 }}>
+      <Link style={{ textDecoration: "none" }} to="/">
+        {siteTitle}
+      </Link>
+    </h1>
+    <h2>{description}</h2>
+  </header>
+)
+
+Header.propTypes = {
+  description: PropTypes.string,
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  description: `Poop chicken`,
+  siteTitle: ``,
+}
+
+export default Header
--- src/components/layout/layout.component.jsx
+++ src/components/layout/layout.component.jsx
@@ -1,22 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from "../header/header.component"
+import "./layout.styles.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
+          description
           title
         }
       }
